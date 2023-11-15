6f2f4e0763a7fbd106949a5f6903441c6f4e4a4a
M. Novrizal <muhammadnovrizal87@gmail.com>
2023-11-15 12:38:50 +0700

    refactoring directory

--- 8. Magic Ball/challenge/android/app/src/main/kotlin/com/artonov/challenge/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.artonov.challenge
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ s15_flash_chat_firebase/lib/screens/login_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flash_chat/components/rounded_button.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flash_chat/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const id = '/login_screen';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  bool _showSpinner = false;
+  late String email;
+  late String password;
+
+  void showSpinner(bool isShowing) {
+    setState(() {
+      _showSpinner = isShowing;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ModalProgressHUD(
+        inAsyncCall: _showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Flexible(
+                child: Hero(
+                  tag: 'logo',
+                  child: SizedBox(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                style: kTextFieldTextStyle,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your email',
+                ),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                style: kTextFieldTextStyle,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password',
+                ),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                color: Colors.lightBlueAccent,
+                title: 'Log in',
+                onPressed: () async {
+                  showSpinner(true);
+                  try {
+                    var userAccount = await _auth.signInWithEmailAndPassword(
+                        email: email, password: password);
+                    if (userAccount != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+
+                    showSpinner(false);
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
